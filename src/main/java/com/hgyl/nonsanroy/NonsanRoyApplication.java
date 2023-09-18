b1952555a806bb56a0f2ea9c71fc04253d4498ac
royonem <roywmyung@gmail.com>
2023-09-18 18:52:39 +0900

    Roy: Foundation: 1st Draft

---
+++ src/main/java/com/hgyl/nonsanroy/NonsanRoyApplication.java
@@ -0,0 +1,13 @@
+package com.hgyl.nonsanroy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonsanRoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonsanRoyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgyl/nonsanroy/NonsanRoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgyl.nonsanroy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonsanRoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

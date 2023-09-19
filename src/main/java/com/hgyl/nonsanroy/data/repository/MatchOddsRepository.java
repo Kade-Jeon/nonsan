4e3a7c92277d4957f762cc4e58818ca2b5bf44d0
royonem <roywmyung@gmail.com>
2023-09-19 17:48:27 +0900

    Roy: Repository: Created 2 Interfaces(MatchOdds, SportsMatch) + fixed application.properties

---
+++ src/main/java/com/hgyl/nonsanroy/data/repository/MatchOddsRepository.java
@@ -0,0 +1,7 @@
+package com.hgyl.nonsanroy.data.repository;
+
+import com.hgyl.nonsanroy.data.entity.bet.MatchOdds;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchOddsRepository extends JpaRepository<MatchOdds, Integer> {
+}
---
+++ src/main/java/com/hgyl/nonsanroy/data/repository/SportsMatchRepository.java
@@ -0,0 +1,13 @@
+package com.hgyl.nonsanroy.data.repository;
+
+import com.hgyl.nonsanroy.data.entity.bet.SportsMatch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author 명원식
+ * @note
+ */
+
+public interface SportsMatchRepository extends JpaRepository<SportsMatch, Integer> {
+
+}

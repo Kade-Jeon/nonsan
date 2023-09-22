c36b07ee51ee66fc081dc814535d8413ce2f7873
hazzokko <hazzokko@gmail.com>
2023-09-22 10:26:21 +0900

    처음

---
+++ src/main/java/com/hgyr/blogpd/BlogpdApplication.java
@@ -0,0 +1,13 @@
+package com.hgyr.blogpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogpdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgyr/blogpd/BlogpdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgyr.blogpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
